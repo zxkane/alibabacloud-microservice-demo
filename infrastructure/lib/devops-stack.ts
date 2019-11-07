8fea5944f0ba38f65dbb059adea19e1f4722b0d6
Kane Zhu <kane.mx@gmail.com>
2019-11-07 13:38:29 +0800

    Add buildspec of services for codebuild.

--- infrastructure/lib/devops-stack.ts
+++ infrastructure/lib/devops-stack.ts
@@ -3,6 +3,8 @@ import codebuild = require('@aws-cdk/aws-codebuild');
 import codepipeline = require('@aws-cdk/aws-codepipeline');
 import codepipeline_actions = require('@aws-cdk/aws-codepipeline-actions');
 import s3 = require("@aws-cdk/aws-s3");
+import ecr = require("@aws-cdk/aws-ecr");
+import iam = require("@aws-cdk/aws-iam")
 
 export class DevopsStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -16,7 +18,19 @@ export class DevopsStack extends cdk.Stack {
 
     const token = this.node.tryGetContext('github-token-key') || 'github-token';
 
-    const services = ['cartservice', 'frontend', 'productservice'];
+    const stack = cdk.Stack.of(this);
+    const services = [ {
+            name: 'cartservice',
+            artifact: 'cartservice-provider/target/cartservice-provider-1.0.0-SNAPSHOT.jar'
+        }, 
+        {
+            name: 'frontend',
+            artifact: 'target/frontend-1.0.0-SNAPSHOT.jar'
+        }, 
+        {
+            name: 'productservice',
+            artifact: 'productservice-provider/target/productservice-provider-1.0.0-SNAPSHOT.jar'
+        }];
     for (const service of services) {
         const sourceOutput = new codepipeline.Artifact();
         // TODO use custom web hook for path filtering
@@ -30,12 +44,130 @@ export class DevopsStack extends cdk.Stack {
             trigger: codepipeline_actions.GitHubTrigger.NONE // default: 'WEBHOOK', 'NONE' is also possible for no Source trigger
         });
         
-        const buildProject = new codebuild.PipelineProject(this, `eCommenceProject-${service}`, {
-            buildSpec: codebuild.BuildSpec.fromSourceFilename(`src/${service}/buildspec.yml`),
-            cache: codebuild.Cache.local(codebuild.LocalCacheMode.DOCKER_LAYER, codebuild.LocalCacheMode.CUSTOM)
+        const buildProject = new codebuild.PipelineProject(this, `eCommenceProject-${service.name}`, {
+            buildSpec: codebuild.BuildSpec.fromObject({
+                version: '0.2',
+                phases: {
+                    pre_build: {
+                        commands: [
+                            'echo Build artifacts cleaning...',
+                            `cd src/${service.name}`,
+                            'mvn clean'
+                        ]
+                    },
+                    build: {
+                        commands: [
+                            'echo Build started on `date`',
+                            'mvn install'
+                        ]
+                    },
+                },
+                artifacts: {
+                    files: [
+                        'Dockerfile',
+                        service.artifact
+                    ],
+                    'discard-paths': 'no',
+                    'base-directory': `src/${service.name}`
+                },
+                cache: {
+                    paths: ['/$HOME/.m2/']
+                }
+            }),
+            cache: codebuild.Cache.local(codebuild.LocalCacheMode.DOCKER_LAYER, codebuild.LocalCacheMode.CUSTOM),
+            environment: {
+                buildImage: codebuild.LinuxBuildImage.UBUNTU_14_04_OPEN_JDK_8,
+            }
         });
-        const pipeline = new codepipeline.Pipeline(this, `eCommencePipeline-${service}`, {
-            pipelineName: `eCommencePipeline-${service}`,
+        const buildOutput = new codepipeline.Artifact();
+        const repoName = `ecommence_${service.name}`;
+        new ecr.Repository(this, repoName, {
+            repositoryName: repoName,
+        });
+        const ecrPolicy1 = new iam.PolicyStatement({
+            effect: iam.Effect.ALLOW,
+        });
+        ecrPolicy1.addActions("ecr:BatchCheckLayerAvailability",
+            "ecr:CompleteLayerUpload",
+            "ecr:GetAuthorizationToken",
+            "ecr:InitiateLayerUpload",
+            "ecr:PutImage",
+            "ecr:UploadLayerPart"
+        );
+        ecrPolicy1.addResources(`arn:${stack.partition}:ecr:${stack.region}:${stack.account}:repository/${repoName}`);
+        const ecrPolicy2 = new iam.PolicyStatement({
+            effect: iam.Effect.ALLOW,
+        });
+        ecrPolicy2.addActions(
+            "ecr:GetAuthorizationToken",
+        );
+        ecrPolicy2.addAllResources();
+        const ecrRole = new iam.Role(this, `ECRRole-eCommence-${service.name}`, {
+            assumedBy: new iam.ServicePrincipal("codebuild.amazonaws.com"),
+            inlinePolicies: {
+                ecr: new iam.PolicyDocument({
+                    statements: [ecrPolicy1, ecrPolicy2]
+                }),
+            }
+        });
+        const deployProject = new codebuild.PipelineProject(this, `eCommenceProject-Image-${service.name}`, {
+            buildSpec: codebuild.BuildSpec.fromObject({
+                version: '0.2',
+                phases: {
+                    install: {
+                        'runtime-versions': {
+                            docker: '18'
+                        }
+                    },
+                    pre_build: {
+                        commands: [
+                            'echo Logging in to Amazon ECR...',
+                            '$(aws ecr get-login --no-include-email --region $AWS_DEFAULT_REGION)'
+                        ]
+                    },
+                    build: {
+                        commands: [
+                            'echo Build started on `date`',
+                            'echo Building the Docker image...',
+                            'docker build -t $IMAGE_REPO_NAME:$IMAGE_TAG .',
+                            'export IAMGE_TAG="$IAMGE_TAG"_"$(date \'+%Y%m%d%H%M%S\')"',
+                            'docker tag $IMAGE_REPO_NAME:$IMAGE_TAG $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME:$IMAGE_TAG',
+                            'docker tag $IMAGE_REPO_NAME:$IMAGE_TAG $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME:latest'
+                        ]
+                    },
+                    post_build: {
+                        commands: [
+                            'echo Build completed on `date`',
+                            'echo Pushing the Docker image...',
+                            'docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME:$IMAGE_TAG',
+                            'docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME:latest'
+                        ]
+                    }  
+                }
+            }),
+            cache: codebuild.Cache.local(codebuild.LocalCacheMode.DOCKER_LAYER, codebuild.LocalCacheMode.CUSTOM),
+            environment: {
+                buildImage: codebuild.LinuxBuildImage.STANDARD_2_0,
+                privileged: true,
+            },
+            environmentVariables: {
+                AWS_DEFAULT_REGION: {
+                    value: stack.region
+                },
+                AWS_ACCOUNT_ID: {
+                    value: stack.account
+                },
+                IMAGE_TAG: {
+                    value: '1.0.0.SNAPSHOT'
+                },
+                IMAGE_REPO_NAME: {
+                    value: repoName 
+                }
+            },
+            role: ecrRole,
+        }); 
+        const pipeline = new codepipeline.Pipeline(this, `eCommencePipeline-${service.name}`, {
+            pipelineName: `eCommencePipeline-${service.name}`,
             artifactBucket: devopsBucket,
             stages: [
                 {
@@ -47,9 +179,18 @@ export class DevopsStack extends cdk.Stack {
                     actions: [new codepipeline_actions.CodeBuildAction({
                         actionName: 'buildSource',
                         input: sourceOutput,
-                        project: buildProject
+                        project: buildProject,
+                        outputs: [buildOutput]
                     })]
-                }
+                },
+                {
+                    stageName: 'image_build_deploy',
+                    actions: [new codepipeline_actions.CodeBuildAction({
+                        actionName: 'publishImage',
+                        input: buildOutput,
+                        project: deployProject,
+                    })]
+                },
             ]
         });
     }
