aea5d8fa1d7e324279a451ccf5c157b89f6e8717
Kane Zhu <kane.mx@gmail.com>
2019-11-12 18:03:25 +0800

    Reformat code of cluster stack.

--- infrastructure/lib/cluster-stack.ts
+++ infrastructure/lib/cluster-stack.ts
@@ -8,45 +8,45 @@ interface ClusterProps extends cdk.StackProps {
     readonly vpc: ec2.IVpc;
 }
 export class ClusterStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props: ClusterProps) {
-    super(scope, id, props);
+    constructor(scope: cdk.Construct, id: string, props: ClusterProps) {
+        super(scope, id, props);
 
-    // The code that defines your stack goes here
-    const cluster = new ecs.Cluster(this, `eCommenceCluster`, {
-        defaultCloudMapNamespace: {
-          name: 'eCommenceCloudMapNamesapce',
-          type: servicediscovery.NamespaceType.DNS_PRIVATE,
-          vpc: props.vpc
-        },
-        vpc: props.vpc
-    });
+        // The code that defines your stack goes here
+        const cluster = new ecs.Cluster(this, `eCommenceCluster`, {
+            defaultCloudMapNamespace: {
+                name: 'eCommenceCloudMapNamesapce',
+                type: servicediscovery.NamespaceType.DNS_PRIVATE,
+                vpc: props.vpc
+            },
+            vpc: props.vpc
+        });
 
-    const nacosTaskDefinition = new ecs.FargateTaskDefinition(this, `NacosTask`, {
-        // https://docs.aws.amazon.com/AmazonECS/latest/developerguide/task-cpu-memory-error.html
-        memoryLimitMiB: 2048, 
-        cpu: 512
-      });
-      const nacosContainer = nacosTaskDefinition.addContainer(`NacosContainer`, {
-        // Use an image from previous built image
-        image: ecs.ContainerImage.fromRegistry('nacos/nacos-server:latest'),
-        // ... other options here ...
-        environment: {
-            PREFER_HOST_MODE: 'hostname',
-            MODE: 'standalone'
-        }
-      });
-      nacosContainer.addPortMappings({
-        containerPort: 8848,
-      });
-      const nacosService = new ecs.FargateService(this, `NacosService`, {
-        cluster,
-        cloudMapOptions: {
-          dnsRecordType: servicediscovery.DnsRecordType.A,
-          dnsTtl: cdk.Duration.seconds(10),
-          failureThreshold: 2,
-        },
-        taskDefinition: nacosTaskDefinition,
-        desiredCount: 1
-      });
-  }
+        const nacosTaskDefinition = new ecs.FargateTaskDefinition(this, `NacosTask`, {
+            // https://docs.aws.amazon.com/AmazonECS/latest/developerguide/task-cpu-memory-error.html
+            memoryLimitMiB: 2048,
+            cpu: 512
+        });
+        const nacosContainer = nacosTaskDefinition.addContainer(`NacosContainer`, {
+            // Use an image from previous built image
+            image: ecs.ContainerImage.fromRegistry('nacos/nacos-server:latest'),
+            // ... other options here ...
+            environment: {
+                PREFER_HOST_MODE: 'hostname',
+                MODE: 'standalone'
+            }
+        });
+        nacosContainer.addPortMappings({
+            containerPort: 8848,
+        });
+        const nacosService = new ecs.FargateService(this, `NacosService`, {
+            cluster,
+            cloudMapOptions: {
+                dnsRecordType: servicediscovery.DnsRecordType.A,
+                dnsTtl: cdk.Duration.seconds(10),
+                failureThreshold: 2,
+            },
+            taskDefinition: nacosTaskDefinition,
+            desiredCount: 1
+        });
+    }
 }
