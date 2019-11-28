f49b4833097e72a06e13ffb5f2901d4da012e2fd
Kane Zhu <kane.mx@gmail.com>
2019-11-28 17:55:59 +0800

    Refactor cluster stack for supporting canary deployment.

--- infrastructure/lib/cluster-stack.ts
+++ infrastructure/lib/cluster-stack.ts
@@ -11,6 +11,7 @@ import servicediscovery = require('@aws-cdk/aws-servicediscovery');
 import ssm = require('@aws-cdk/aws-ssm');
 import route53 = require('@aws-cdk/aws-route53');
 import route53Targets = require('@aws-cdk/aws-route53-targets');
+import { MessageChannel } from 'worker_threads';
 
 interface ClusterProps extends cdk.StackProps {
     readonly vpc: ec2.IVpc;
@@ -160,185 +161,218 @@ export class ClusterStack extends cdk.Stack {
                 cpu: 1024,
                 memory: 2048,
                 replicas: 2,
-                ports: [8082]
+                ports: [8082],
+                appmesh: true,
+                canary: true,
             }
         ];
+
+        const uid = 1337;
+        const envoyIngressPort = 15000;
+        const envoyEgressPort = 15001;
+
         for (const service of eCommenceServices) {
-            const taskRole = new iam.Role(this, `TaskRole-${service.name}`, {
-                assumedBy: new iam.ServicePrincipal("ecs-tasks.amazonaws.com"),
-                managedPolicies: [
-                  iam.ManagedPolicy.fromAwsManagedPolicyName('AWSXRayDaemonWriteAccess'),
-                  iam.ManagedPolicy.fromAwsManagedPolicyName('AWSAppMeshEnvoyAccess'),
-                ]
-            });
-            const executionRole = new iam.Role(this, `ExecutionRole-${service.name}`, {
-                assumedBy: new iam.ServicePrincipal("ecs-tasks.amazonaws.com"),
-                managedPolicies: [
-                  iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonEC2ContainerRegistryReadOnly'),
-                ]
-            });
-            const envoyContainerName = `envoy-${service.name}`;
-            var proxyConfiguration = null;
-            const uid = 1337;
-            const envoyIngressPort = 15000;
-            const envoyEgressPort = 15001;
-            if (service.appmesh) {
-                proxyConfiguration = ecs.ProxyConfigurations.appMeshProxyConfiguration({
-                    containerName: envoyContainerName,
-                    properties: {
-                        appPorts: service.ports,
-                        proxyIngressPort: envoyIngressPort,
-                        proxyEgressPort: envoyEgressPort,
-                        egressIgnoredIPs: [
-                            '169.254.170.2',
-                            '169.254.169.254',
-                        ],
-                        ignoredUID: uid,
-                    }
+            const versions = [{
+                name: 'mainline',
+                version: ssm.StringParameter.fromStringParameterAttributes(this, `${service.name}ImageVersion`, {
+                    parameterName: `/prod/eCommence/${service.name}/version/latest`,
+                    // 'version' can be specified but is optional.
+                }).stringValue
+            },];
+
+            const routeTargets = [];
+            const targets: elbv2.IApplicationLoadBalancerTarget[] = [];
+            for (const version of versions) {
+                const taskRole = new iam.Role(this, `TaskRole-${service.name}`, {
+                    assumedBy: new iam.ServicePrincipal("ecs-tasks.amazonaws.com"),
+                    managedPolicies: [
+                        iam.ManagedPolicy.fromAwsManagedPolicyName('AWSXRayDaemonWriteAccess'),
+                        iam.ManagedPolicy.fromAwsManagedPolicyName('AWSAppMeshEnvoyAccess'),
+                    ]
                 });
-            }
-            const microServiceTaskDefinition = new ecs.FargateTaskDefinition(this, `${service.name}Task`, {
-                memoryLimitMiB: service.memory,
-                cpu: service.cpu,
-                taskRole,
-                executionRole,
-                family: service.name,
-                proxyConfiguration,
-            });
-            const xrayDaemon = microServiceTaskDefinition.addContainer(`x-ray-for-${service.name}`, {
-                image: ecs.ContainerImage.fromRegistry('amazon/aws-xray-daemon'),
-                essential: true,
-                cpu: 32,
-                memoryReservationMiB: 256,
-                healthCheck: {
-                    command: [
-                        "CMD-SHELL",
-                        "timeout 1 /bin/bash -c '</dev/tcp/localhost/2000 && </dev/udp/localhost/2000'"
-                    ],
-                    startPeriod: cdk.Duration.seconds(10),
-                    interval: cdk.Duration.seconds(5),
-                    timeout: cdk.Duration.seconds(2),
-                    retries: 1
-                },
-                user: String(uid),
-            });
-            const imageVersion = ssm.StringParameter.fromStringParameterAttributes(this, `${service.name}ImageVersion`, {
-                parameterName: `/prod/eCommence/${service.name}/version/latest`,
-                // 'version' can be specified but is optional.
-            }).stringValue;
-            const microServiceContainer = microServiceTaskDefinition.addContainer(`${service.name}Container`, {
-                // Use an image from previous built image
-                image: ecs.ContainerImage.fromEcrRepository(
-                    ecr.Repository.fromRepositoryName(this, `${service.name}EcrRepo`, `${repoPrefix}${service.name}`),
-                    imageVersion),
-                // ... other options here ...
-                environment: Object.assign({'VERSION': imageVersion}, service.environments),
-                logging: ecs.LogDrivers.awsLogs({
-                    streamPrefix: service.name,
-                    datetimeFormat: '%Y-%m-%d %H:%M:%S',
-                    logGroup: logGroup,
-                }),
-            });
-            microServiceContainer.addContainerDependencies({
-                container: xrayDaemon,
-                condition: ecs.ContainerDependencyCondition.HEALTHY,
-            });
-            if (service.ports) {
-                for (const port of service.ports) {
-                    microServiceContainer.addPortMappings({
-                        containerPort: port,
+                const executionRole = new iam.Role(this, `ExecutionRole-${service.name}`, {
+                    assumedBy: new iam.ServicePrincipal("ecs-tasks.amazonaws.com"),
+                    managedPolicies: [
+                        iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonEC2ContainerRegistryReadOnly'),
+                    ]
+                });
+
+                const envoyContainerName = `envoy-${service.name}-${version.name}`;
+                var proxyConfiguration = null;
+                if (service.appmesh) {
+                    proxyConfiguration = ecs.ProxyConfigurations.appMeshProxyConfiguration({
+                        containerName: envoyContainerName,
+                        properties: {
+                            appPorts: service.ports,
+                            proxyIngressPort: envoyIngressPort,
+                            proxyEgressPort: envoyEgressPort,
+                            egressIgnoredIPs: [
+                                '169.254.170.2',
+                                '169.254.169.254',
+                            ],
+                            ignoredUID: uid,
+                        }
                     });
                 }
-            }
-            const microServiceService = new ecs.FargateService(this, `${service.name}Service`, {
-                cluster,
-                cloudMapOptions: {
+
+                const microServiceTaskDefinition = new ecs.FargateTaskDefinition(this, `task-${service.name}-${version.name}`, {
+                    memoryLimitMiB: service.memory,
+                    cpu: service.cpu,
+                    taskRole,
+                    executionRole,
+                    family: service.name,
+                    proxyConfiguration,
+                });
+                const xrayDaemon = microServiceTaskDefinition.addContainer(`x-ray-for-${service.name}-${version.name}`, {
+                    image: ecs.ContainerImage.fromRegistry('amazon/aws-xray-daemon'),
+                    essential: true,
+                    cpu: 32,
+                    memoryReservationMiB: 256,
+                    healthCheck: {
+                        command: [
+                            "CMD-SHELL",
+                            "timeout 1 /bin/bash -c '</dev/tcp/localhost/2000 && </dev/udp/localhost/2000'"
+                        ],
+                        startPeriod: cdk.Duration.seconds(10),
+                        interval: cdk.Duration.seconds(5),
+                        timeout: cdk.Duration.seconds(2),
+                        retries: 1
+                    },
+                    user: String(uid),
+                });
+                const microServiceContainer = microServiceTaskDefinition.addContainer(`container-${service.name}-${version.name}`, {
+                    // Use an image from previous built image
+                    image: ecs.ContainerImage.fromEcrRepository(
+                        ecr.Repository.fromRepositoryName(this, `${service.name}EcrRepo`, `${repoPrefix}${service.name}`),
+                        version.version),
+                    // ... other options here ...
+                    environment: Object.assign({ 'VERSION': version.version }, service.environments),
+                    logging: ecs.LogDrivers.awsLogs({
+                        streamPrefix: service.name,
+                        datetimeFormat: '%Y-%m-%d %H:%M:%S',
+                        logGroup: logGroup,
+                    }),
+                });
+
+                microServiceContainer.addContainerDependencies({
+                    container: xrayDaemon,
+                    condition: ecs.ContainerDependencyCondition.HEALTHY,
+                });
+                if (service.ports) {
+                    for (const port of service.ports) {
+                        microServiceContainer.addPortMappings({
+                            containerPort: port,
+                        });
+                    }
+                }
+
+                const microServiceService = new ecs.FargateService(this, `service-${service.name}-${version.name}`, {
+                    cluster,
+                    cloudMapOptions: {
+                        name: service.name,
+                        dnsRecordType: servicediscovery.DnsRecordType.A,
+                        dnsTtl: cdk.Duration.seconds(10),
+                        failureThreshold: 2,
+                    },
+                    taskDefinition: microServiceTaskDefinition,
+                    desiredCount: service.replicas
+                });
+                for (const nacosPort of nacosPorts) {
+                    nacosService.connections.allowFrom(microServiceService.connections,
+                        ec2.Port.tcp(nacosPort), `request from ${service.name}`);
+                }
+                microServices.push({
                     name: service.name,
-                    dnsRecordType: servicediscovery.DnsRecordType.A,
-                    dnsTtl: cdk.Duration.seconds(10),
-                    failureThreshold: 2,
-                },
-                taskDefinition: microServiceTaskDefinition,
-                desiredCount: service.replicas
-            });
-            for (const nacosPort of nacosPorts) {
-                nacosService.connections.allowFrom(microServiceService.connections,
-                    ec2.Port.tcp(nacosPort), `request from ${service.name}`);
+                    service: microServiceService,
+                    ports: service.ports
+                });
+
+                if (service.appmesh) {
+                    microServiceService.connections.allowInternally(ec2.Port.tcp(envoyIngressPort), 'envoy ingress port');
+                    for (const port of service.ports) {
+                        microServiceService.connections.allowInternally(ec2.Port.tcp(port), `service port ${port}`);
+                    }
+
+                    const node = new appmesh.VirtualNode(this, `node-${service.name}-${version.name}`, {
+                        mesh,
+                        cloudMapService: microServiceService.cloudMapService,
+                        listener: {
+                            portMapping: {
+                                port: service.ports[0],
+                                protocol: appmesh.Protocol.HTTP,
+                            },
+                        },
+                    });
+
+                    routeTargets.push({
+                        virtualNode: node,
+                        weight: 10, 
+                    });
+
+                    const envoyContainer = microServiceTaskDefinition.addContainer(envoyContainerName, {
+                        image: ecs.ContainerImage.fromRegistry(`840364872350.dkr.ecr.${stack.region}.amazonaws.com/aws-appmesh-envoy:v1.12.1.1-prod`),
+                        essential: true,
+                        environment: {
+                            APPMESH_VIRTUAL_NODE_NAME: `mesh/${mesh.meshName}/virtualNode/${node.virtualNodeName}`,
+                            ENABLE_ENVOY_XRAY_TRACING: '1',
+                        },
+                        healthCheck: {
+                            command: [
+                                "CMD-SHELL",
+                                "curl -s http://localhost:9901/server_info | grep state | grep -q LIVE"
+                            ],
+                            startPeriod: cdk.Duration.seconds(30),
+                            interval: cdk.Duration.seconds(5),
+                            timeout: cdk.Duration.seconds(2),
+                            retries: 2
+                        },
+                        user: String(uid),
+                    });
+                    envoyContainer.addPortMappings({
+                        containerPort: envoyIngressPort
+                    }, {
+                        containerPort: envoyEgressPort
+                    });
+                    microServiceContainer.addContainerDependencies({
+                        container: envoyContainer,
+                        condition: ecs.ContainerDependencyCondition.HEALTHY,
+                    });
+                    xrayDaemon.addContainerDependencies({
+                        container: envoyContainer,
+                        condition: ecs.ContainerDependencyCondition.HEALTHY,
+                    });
+                }
+
+                if (service.expose) {
+                    targets.push(microServiceService.loadBalancerTarget({
+                        containerName: microServiceContainer.containerName,
+                        containerPort: service.ports[0],
+                        protocol: ecs.Protocol.TCP
+                    }));    
+                }
             }
-            microServices.push({
-                name: service.name,
-                service: microServiceService,
-                ports: service.ports
-            });
 
             /**
              * Add app mesh
              */
-            if (service.appmesh){
-                microServiceService.connections.allowInternally(ec2.Port.tcp(envoyIngressPort), 'envoy ingress port');
-                for (const port of service.ports)
-                    microServiceService.connections.allowInternally(ec2.Port.tcp(port), `service port ${port}`);
-                const node = new appmesh.VirtualNode(this, `node-${service.name}`, {
-                    mesh,
-                    cloudMapService: microServiceService.cloudMapService,
-                    listener: {
-                      portMapping: {
-                        port: service.ports[0],
-                        protocol: appmesh.Protocol.HTTP,
-                      },
-                    },
-                });
-                const envoyContainer = microServiceTaskDefinition.addContainer(envoyContainerName, {
-                    image: ecs.ContainerImage.fromRegistry(`840364872350.dkr.ecr.${stack.region}.amazonaws.com/aws-appmesh-envoy:v1.12.1.1-prod`),
-                    essential: true,
-                    environment: {
-                        APPMESH_VIRTUAL_NODE_NAME: `mesh/${mesh.meshName}/virtualNode/${node.virtualNodeName}`,
-                        ENABLE_ENVOY_XRAY_TRACING: '1',
-                    },
-                    healthCheck: {
-                        command: [
-                            "CMD-SHELL",
-                            "curl -s http://localhost:9901/server_info | grep state | grep -q LIVE"
-                        ],
-                        startPeriod: cdk.Duration.seconds(30),
-                        interval: cdk.Duration.seconds(5),
-                        timeout: cdk.Duration.seconds(2),
-                        retries: 2
-                    },
-                    user: String(uid)
-                });
-                envoyContainer.addPortMappings({
-                    containerPort: envoyIngressPort
-                }, {
-                    containerPort: envoyEgressPort
-                });
-                microServiceContainer.addContainerDependencies({
-                    container: envoyContainer,
-                    condition: ecs.ContainerDependencyCondition.HEALTHY,
-                });
-                xrayDaemon.addContainerDependencies({
-                    container: envoyContainer,
-                    condition: ecs.ContainerDependencyCondition.HEALTHY, 
-                });
-
+            if (service.appmesh && service.canary) {
                 const router = mesh.addVirtualRouter(`router-${service.name}`, {
                     listener: {
-                      portMapping: {
-                        port: service.ports[0],
-                        protocol: appmesh.Protocol.HTTP,
-                      }
+                        portMapping: {
+                            port: service.ports[0],
+                            protocol: appmesh.Protocol.HTTP,
+                        }
                     }
                 });
                 router.addRoute(`route-${service.name}`, {
-                    routeTargets: [
-                      {
-                        virtualNode: node,
-                        weight: 10,
-                      },
-                    ],
+                    routeTargets,
                     prefix: `/`,
                     routeType: appmesh.RouteType.HTTP,
-                  });
+                });
+                mesh.addVirtualService(`virtual-service-${service.name}`, {
+                    virtualServiceName: service.name,
+                    virtualRouter: router,
+                });
             }
 
             if (service.expose) {
@@ -347,11 +381,7 @@ export class ClusterStack extends cdk.Stack {
                     port: service.ports[0],
                     pathPattern: service.expose.path,
                     priority: service.expose.priority,
-                    targets: [microServiceService.loadBalancerTarget({
-                        containerName: microServiceContainer.containerName,
-                        containerPort: service.ports[0],
-                        protocol: ecs.Protocol.TCP
-                      })],
+                    targets,
                     deregistrationDelay: cdk.Duration.seconds(5),
                 });
                 listener443.addTargetGroups('Targets', {
